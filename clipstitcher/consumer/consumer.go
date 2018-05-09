8ca6660da4940014d1b3ec5aa6522d48bd9483ae
Austin Davis <austindavis4@gmail.com>
2018-05-08 23:42:13 -0600

    Communication between producer and consumer works.

--- clipstitcher/consumer/consumer.go
+++ clipstitcher/consumer/consumer.go
@@ -10,10 +10,14 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 )
 
+type snsMessage struct {
+	Message string `json:"Message"`
+}
+
 type clipMessage struct {
 	VideoLinks       []string `json:"videoLinks"`
 	VideoDescription string   `json:"videoDescription"`
-	ChannelName      string   `json:"channelName "`
+	ChannelName      string   `json:"channelName"`
 	ReceiptHandle    *string
 }
 
@@ -44,6 +48,7 @@ func NewConsumerService(queEndpoint string, queueName string) (consumerService, 
 
 func (cService consumerService) GetMessage() (clipMessage, error) {
 	cMessage := clipMessage{}
+	snsWrapper := snsMessage{}
 	messageConfig := &sqs.ReceiveMessageInput{
 		QueueUrl: cService.QueueURL,
 	}
@@ -52,29 +57,34 @@ func (cService consumerService) GetMessage() (clipMessage, error) {
 	if err != nil {
 		return cMessage, err
 	}
-	fmt.Printf("%+v\n", messageOutput.Messages[0])
 	if len(messageOutput.Messages) == 0 {
 		err = errors.New("No messages found in sqs que")
 		return cMessage, err
 	}
-	cMessage.ReceiptHandle = messageOutput.Messages[0].ReceiptHandle
-	message := *messageOutput.Messages[0].Body
-	err = json.Unmarshal([]byte(message), &cMessage)
+	wrapperMessage := *messageOutput.Messages[0].Body
+	err = json.Unmarshal([]byte(wrapperMessage), &snsWrapper)
 	if err != nil {
 		return cMessage, err
 	}
-	fmt.Println("Message recieved from sqs" + cMessage.ChannelName)
+	actualMessage := snsWrapper.Message
+	err = json.Unmarshal([]byte(actualMessage), &cMessage)
+	if err != nil {
+		return cMessage, err
+	}
+	cMessage.ReceiptHandle = messageOutput.Messages[0].ReceiptHandle
+	fmt.Println("Message recieved from sqs")
 	return cMessage, nil
 }
 
-func (cService consumerService) DeleteMessage(message clipMessage) {
+func (cService consumerService) DeleteMessage(message clipMessage) error {
 	deleteInput := &sqs.DeleteMessageInput{
 		QueueUrl:      cService.QueueURL,
 		ReceiptHandle: message.ReceiptHandle,
 	}
 	_, err := cService.SqsClient.DeleteMessage(deleteInput)
 	if err != nil {
-		fmt.Println("err is" + err.Error())
+		return err
 	}
 	fmt.Println("message deleted from sqs")
+	return nil
 }
--- clipstitcher/main.go
+++ clipstitcher/main.go
@@ -28,7 +28,7 @@ func logAndExit(err error) {
 
 func init() {
 	youtubeAuth = os.Getenv("YOUTUBE_AUTH")
-	if twitchChannelName == "" {
+	if youtubeAuth == "" {
 		fmt.Println("YOUTUBE_AUTH ENV var was not set.")
 		os.Exit(1)
 	}
@@ -45,14 +45,15 @@ func init() {
 func main() {
 	fmt.Println("clip sticher started")
 	start := time.Now()
-	consumerService := consumer.NewConsumerService(consumerEndpoint, consumerName)
-	clipMessage := consumerService.GetMessage()
+	consumerService, err := consumer.NewConsumerService(consumerEndpoint, consumerName)
+	logAndExit(err)
+	clipMessage, err := consumerService.GetMessage()
 	logAndExit(err)
 	fmt.Println("starting ffmpeg")
 	ffmpegReader, err := stitcher.StitchClips(clipMessage.VideoLinks)
 	logAndExit(err)
 	fmt.Println("starting upload")
-	err = uploader.Upload(ffmpegReader, youtubeAuth, clipMessage.VideoDescription, clipMessage.twitchChannelName)
+	err = uploader.Upload(ffmpegReader, youtubeAuth, clipMessage.VideoDescription, clipMessage.ChannelName)
 	if err != nil {
 		logAndExit(err)
 	}
@@ -60,7 +61,8 @@ func main() {
 	elapsed := time.Since(start)
 	sitcherOutput := strings.Replace(string(stitcher.Logs), "%", "%%", -1)
 	fmt.Println(sitcherOutput)
-	consumerService.DeleteMessage(message)
+	err = consumerService.DeleteMessage(clipMessage)
+	logAndExit(err)
 	fmt.Println("total execution time took", elapsed)
 	os.Exit(0)
 }
